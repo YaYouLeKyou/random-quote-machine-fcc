c31dc5b72d69e2b08e7db5541c11f3532369875c
Yayou <78508960+YaYouLeKyou@users.noreply.github.com>
2021-09-09 21:21:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+function App() {
+  const [quotes, setQuotes] = React.useState([]);
+  const [randomQuote, setRandomQuote] = React.useState('');
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://type.fit/api/quotes');
+      const data = await response.json();
+
+      setQuotes(data);
+      let randIndex = Math.floor(Math.random() * data.length);
+      setRandomQuote(data[randIndex]);
+    }
+    fetchData();
+  }, []);
+
+  const getNewQuote = () => {
+    let randIndex = Math.floor(Math.random() * quotes.length);
+    setRandomQuote(quotes[randIndex]);
+  };
+  return (
+    <div className="container pt-5">
+      <div className="jumbotron">
+        <div className="card">
+          <div className="card-header">Inspirational quotes</div>
+          <div className="card-body">
+            {randomQuote ? (
+              <>
+                <h5 className="card-title">
+                  - {randomQuote.author || 'no author'}
+                </h5>
+                <p>"{randomQuote.text}"</p>
+              </>
+            ) : (
+              <h2>loading</h2>
+            )}
+
+            <div className="column">
+              <button onClick={getNewQuote} className="btn btn-primary">
+                New Quote
+              </button>
+              <a
+                href={
+                  'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+                  encodeURIComponent(
+                    '"' + randomQuote.text + '" ' + randomQuote.author
+                  )
+                }
+                target="_blank"
+                className="btn btn-warning"
+              >
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a
+                href={
+                  'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+                  encodeURIComponent(randomQuote.author) +
+                  '&content=' +
+                  encodeURIComponent(randomQuote.text) +
+                  '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+                }
+                target="_blank"
+                className="btn btn-danger"
+              >
+                <i className="fab fa-tumblr"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
